b2725045ac12564b7040c8f56531a3e029780843
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-06-24 11:21:30 +0530

    1. Fixed issues in profile card 2. Added slight styling to contact form items

--- src/components/card/profile-card.js
+++ src/components/card/profile-card.js
@@ -14,23 +14,27 @@ class ProfileCard extends React.Component
     let github_username = this.props.github_username;
     let linkedin_username = this.props.linkedin_username;
     let twitter_username = this.props.twitter_username;
-    let footer_position = "absolute";
-    let info_width = "75%";
+    //let footer_position = "absolute";
+    //let info_width = "75%";
+    let info_class = "profile-card-info";
+    let footer_class = "profile-card-footer";
     if(this.props.image == null || this.props.image == "")
     {
-      footer_position = "relative";
-      info_width = "100%";
+      //footer_position = "relative";
+      //info_width = "100%";
+      info_class += "-no-image";
+      footer_class += "-no-image";
     }
     return (
       <div className="profile-card">
         { this.props.image != null &&
           <img className="profile-card-image" src={this.props.image}/>
         }
-        <div className="profile-card-info" style={{width: info_width}}>
+        <div className={info_class}>
           <div className="profile-card-content">
             {this.props.content}
           </div>
-          <div className="profile-card-footer" style={{position: footer_position}}>
+          <div className={footer_class}>
             <p style={{margin: 0}}>
               <strong>Follow me:</strong>
               { this.props.linkedin_username != null &&
