17d8902edf4d180c831fcda1ddaa909b34f30010
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-06-28 18:41:03 +0530

    Added alt and lopen new pages with noopener attribute

--- src/components/card/profile-card.js
+++ src/components/card/profile-card.js
@@ -31,6 +31,7 @@ class ProfileCard extends React.Component
           <img
             className="profile-card-image"
             src={this.props.image}
+            alt={"Profile image of @"+twitter_username}
           />
         }
         <div className={info_class}>
@@ -41,13 +42,13 @@ class ProfileCard extends React.Component
             <p style={{margin: 0}}>
               <strong>Follow me:</strong>
               { this.props.linkedin_username != null &&
-                <a href={"https://www.linkedin.com/in/"+linkedin_username} target="_blank"><i className="fab fa-linkedin-in" style={{color: 'hsl(201, 100%, 35%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://www.linkedin.com/in/"+linkedin_username} target="_blank" rel="noopener"><i className="fab fa-linkedin-in" style={{color: 'hsl(201, 100%, 35%)', padding: '0.2rem 0.5rem'}}></i></a>
               }
               { this.props.twitter_username != null &&
-                <a href={"https://twitter.com/"+twitter_username} target="_blank"><i className="fab fa-twitter" style={{color: 'hsl(203, 89%, 53%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://twitter.com/"+twitter_username} target="_blank" rel="noopener"><i className="fab fa-twitter" style={{color: 'hsl(203, 89%, 53%)', padding: '0.2rem 0.5rem'}}></i></a>
               }
               { this.props.github_username != null &&
-                <a href={"https://github.com/"+github_username} target="_blank"><i className="fab fa-github" style={{color: 'hsl(214, 50%, 50%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://github.com/"+github_username} target="_blank" rel="noopener"><i className="fab fa-github" style={{color: 'hsl(214, 50%, 50%)', padding: '0.2rem 0.5rem'}}></i></a>
               }
               <Link to="/feed.xml"><i className="fa fa-rss" style={{color: 'hsl(19, 89%, 54%)', padding: '0.2rem 0.5rem'}}></i></Link>
             </p>
