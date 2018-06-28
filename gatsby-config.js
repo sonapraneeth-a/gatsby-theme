1ae0b861982b82b5f0b521eaa24772fca1faf672
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-06-28 11:13:53 +0530

    Added nprogress plugin to the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -157,6 +157,15 @@ module.exports = {
       }
     }*/
     {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `tomato`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
       resolve: "gatsby-plugin-feed",
       options: {
         setup(ref)
