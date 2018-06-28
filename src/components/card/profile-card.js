6ed064d889d3b63e68cf78c5d5b8c49544c71f16
sonapraneeth-a <sonapraneeth.akula@gmail.com>
2018-06-28 19:55:09 +0530

    Fixed the aria label for links in profile card

--- src/components/card/profile-card.js
+++ src/components/card/profile-card.js
@@ -42,15 +42,15 @@ class ProfileCard extends React.Component
             <p style={{margin: 0}}>
               <strong>Follow me:</strong>
               { this.props.linkedin_username != null &&
-                <a href={"https://www.linkedin.com/in/"+linkedin_username} target="_blank" rel="noopener"><i className="fab fa-linkedin-in" style={{color: 'hsl(201, 100%, 35%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://www.linkedin.com/in/"+linkedin_username} target="_blank" rel="noopener" aria-label={"Linkedin profile of @"+linkedin_username}><i className="fab fa-linkedin-in" style={{color: 'hsl(201, 100%, 35%)', padding: '0.2rem 0.5rem'}} aria-hidden="true"></i></a>
               }
               { this.props.twitter_username != null &&
-                <a href={"https://twitter.com/"+twitter_username} target="_blank" rel="noopener"><i className="fab fa-twitter" style={{color: 'hsl(203, 89%, 53%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://twitter.com/"+twitter_username} target="_blank" rel="noopener" aria-label={"Twitter profile of @"+twitter_username}><i className="fab fa-twitter" style={{color: 'hsl(203, 89%, 53%)', padding: '0.2rem 0.5rem'}} aria-hidden="true"></i></a>
               }
               { this.props.github_username != null &&
-                <a href={"https://github.com/"+github_username} target="_blank" rel="noopener"><i className="fab fa-github" style={{color: 'hsl(214, 50%, 50%)', padding: '0.2rem 0.5rem'}}></i></a>
+                <a href={"https://github.com/"+github_username} target="_blank" rel="noopener" aria-label={"Github profile of @"+github_username}><i className="fab fa-github" style={{color: 'hsl(214, 50%, 50%)', padding: '0.2rem 0.5rem'}} aria-hidden="true"></i></a>
               }
-              <Link to="/feed.xml"><i className="fa fa-rss" style={{color: 'hsl(19, 89%, 54%)', padding: '0.2rem 0.5rem'}}></i></Link>
+              <Link to="/feed.xml"><i className="fa fa-rss" style={{color: 'hsl(19, 89%, 54%)', padding: '0.2rem 0.5rem'}} aria-hidden="true"></i></Link>
             </p>
           </div>
         </div>
