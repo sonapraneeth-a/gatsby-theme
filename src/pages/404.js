1306c0adc3f5cf63be21ad78c2d7632c2ad905eb
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2018-09-23 11:48:03 +0530

    1. Added a new page layout 2. Moved 404 page to the new page layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,10 @@ import { graphql } from "gatsby";
 import SimpleCard from "../components/card/simple-card.js"
 import HeadMeta from "../components/head/head-meta";
 import SEO from "../components/head/seo";
+import PageLayout from "../components/layouts/page.js";
 import BaseLayout from "../components/layouts/base.js";
 
-class Index extends React.Component
+class NotFound extends React.Component
 {
   constructor(props)
   {
@@ -17,7 +18,7 @@ class Index extends React.Component
   render()
   {
     return (
-      <BaseLayout location={this.props.location}>
+      <PageLayout location={this.props.location} icon_name={"fa-exclamation"} title={"404 - Not found"}>
         <HeadMeta
           title={"404 Not found | " + this.props.data.site.siteMetadata.author}
           description={"This is the home page of " + this.props.data.site.siteMetadata.author}
@@ -31,16 +32,15 @@ class Index extends React.Component
           site_name={"Homepage of " + this.props.data.site.siteMetadata.author}
           twitter_username={this.props.data.site.siteMetadata.social.twitter.username}
         />
-        <div className="page-title"><i className="fa fa-exclamation fa-1" aria-hidden={"true"}></i>&nbsp;&nbsp;404 - Not found</div>
         <SimpleCard>
           You have landed in a wrong location. Please <a href={this.props.history}>go back</a> (or) head to <a href={this.props.data.site.siteMetadata.siteUrl}>home</a>!!!
         </SimpleCard>
-      </BaseLayout>
+      </PageLayout>
     )
   }
 }
 
-export default Index;
+export default NotFound;
 
 export const query = graphql`
   query LostQuery
